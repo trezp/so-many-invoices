30bf4dc8220daf1b6dfcaf92d0f4d4aea8a97e95
Treasure Porth <treasure.rose@gmail.com>
2021-04-18 15:06:33 -0700

    Adds react router and routing to switch between invoices

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import Invoice from "./components/invoice/Invoice";
+import data from './invoiceDB.json';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
-      <header>
-        <h1>Super Spectacular Invoicing!</h1>
-      </header>
+      <Router>
+        <div>
+          <header>
+            <h1>Super Spectacular Invoicing!</h1>
+          </header>
+          <ul>
+            {data.map(link => <li key={link.id}><Link to={link.id}>{link.title}</Link></li>)}
+          </ul>
 
-      <Invoice/>
+          <Switch>
+            <ul><Route path="/:id" children={<Invoice />} /></ul>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/invoice/Invoice.js
+++ src/components/invoice/Invoice.js
@@ -1,41 +1,49 @@
 import React from 'react';
 
 import LineItem from '../LineItem.js';
+import {
+  useParams
+} from "react-router-dom";
 
 import data from '../../invoiceDB.json';
 import './Invoice.css';
 
-class Invoice extends React.Component {
-
-  makeListItems (){
-    return data.map((listItem) =>
-      <li key={listItem.id} className="invoice-list-item">
-        <h3>{listItem.title}</h3>
-        <table className="line-item-table">
-          <thead>
-            <tr>
-              <td>Item</td>
-              <td>Amount</td>
-              <td>Notes</td>
-            </tr>
-          </thead>
-          <tbody>
-          <LineItem lineItem={listItem.lineItems}/>
-          </tbody>
-        </table> 
-      </li>);
-  }
-
-  render () {
-    return (
-      <ul>
-        {this.makeListItems()}
-      </ul>
-    )
-  }
-  componentDidMount() {
-    console.log(data);
-  }
+function Invoice () {
+  const { id } = useParams();
+  const invoice = data.find(invoice => invoice.id === id);
+  console.log(id)
+  return (
+    <li key={invoice.id} className="invoice-list-item">
+      <h3>{invoice.title}</h3>
+      <table className="line-item-table">
+        <thead>
+          <tr>
+            <td>Item</td>
+            <td>Amount</td>
+            <td>Notes</td>
+          </tr>
+        </thead>
+        <tbody>
+        <LineItem lineItem={invoice.lineItems}/>
+        </tbody>
+      </table> 
+    </li>);
+  // return data.map((listItem) =>
+  //   <li key={listItem.id} className="invoice-list-item">
+  //     <h3>{listItem.title}</h3>
+  //     <table className="line-item-table">
+  //       <thead>
+  //         <tr>
+  //           <td>Item</td>
+  //           <td>Amount</td>
+  //           <td>Notes</td>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //       <LineItem lineItem={listItem.lineItems}/>
+  //       </tbody>
+  //     </table> 
+  //   </li>);
 }
 
 export default Invoice;
